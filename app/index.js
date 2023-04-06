0c77145c7b782b1a2f5509799d8219f23ba6f204
durotemy <durotemy@gmail.com>
2023-04-06 14:21:09 +0100

    something went wrong

--- app/index.js
+++ app/index.js
@@ -25,13 +25,15 @@ export default function Page() {
   return (
     <View style={styles.container}>
       <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <TextInput
-          style={styles.inputField}
-          placeholder="Enter your text here"
-          onChangeText={handleChange}
-        ></TextInput>
-        <Button title="UpdateGoals" onPress={handlePress} />
+        <Text style={styles.title}>First ReactNative Project</Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.inputField}
+            placeholder="Enter your text here"
+            onChangeText={handleChange}
+          ></TextInput>
+          <Button style={styles.button} title="UpdateGoals" onPress={handlePress} />
+        </View>
         <Text style={styles.subtitle}>My Goals</Text>
         <ScrollView style={styles.div}>
           {goals.map((goal) => (
@@ -50,7 +52,12 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     padding: 24,
-    backgroundColor: "yellow",
+    backgroundColor: "grey",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    width:'100%',
+    paddingBottom:'8',
   },
   main: {
     flex: 1,
@@ -59,26 +66,42 @@ const styles = StyleSheet.create({
     marginHorizontal: "auto",
   },
   title: {
+    marginTop: 40,
+    marginBottom: 20,
+    borderBottomColor:"#FFFFFF",
+    textAlign: "center",
     fontSize: 20,
+    color: "white",
     fontWeight: "bold",
   },
   subtitle: {
     fontSize: 20,
     justifyContent: "center",
-    textAlign: "center",
-    alignItems: "center",
-    color: "red",
+    color: "white",
   },
   inputField: {
-    height: 40,
-    borderColor: "gray",
+    height: 50,
+    width: "70%",
+    borderColor: "thistle",
+    padding:8,
     borderWidth: 1,
+    borderRadius: 3,
+    marginRight:5,
+  },
+  button: {
+    width: "10%",
+    height: 50,
+    backgroundColor: "white",
+    borderRadius: 3,
+    color: "blue",
+    fontSize: 20,
+    fontWeight: "bold",
+
   },
   div: {
-    backgroundColor: "blue",
+    // backgroundColor: "blue",
   },
   goalItem: {
-    color: "blue",
     Margin: 8,
     padding: 8,
     backgroundColor: "white",
